73a7a7304f4df20a28339a0ed27eefbcd1a1e2b1
Govind V R <govindvr02@gmail.com>
2022-01-29 02:18:26 +0530

    user registration

--- app/database.py
+++ app/database.py
@@ -1,15 +1,15 @@
 from app import db
 
-class Item(db.model):
-    id = db.Column(db.Integer, primary_key = True)
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key = True)
     username = db.Column(db.String(length = 50), nullable = False, unique = True)
-    password = db.Column(db.String(length = 50), nullable = False, unique = True)
+    password = db.Column(db.String(length = 50), nullable = False)
     name = db.Column(db.String(length = 50), nullable = False)
     email = db.Column(db.String(length = 50), nullable = False, unique = True)
     phno = db.Column(db.String(length = 20), nullable = False, unique = True)
     education_status = db.Column(db.String(length = 100), nullable = False)
     field = db.Column(db.String(length = 500), nullable = False)
-    projects = db.Column(db.String(length = 1000), nullable = False)
+    projects = db.Column(db.String(length = 1000))
 
     def __repr__(self):
-        return f'User {self.username}
+        return f'User {self.name}'
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, request
+from flask import render_template, request,redirect, flash
 from app import app
 from app.forms import RegisterForm, LoginForm
 from app import db
+from app.database import User
 
 @app.route('/')
 def home():
@@ -11,17 +12,28 @@ def home():
 def login():
     form = LoginForm()
     if request.method == 'POST':
-        user = form.username.data
-        psswd = form.password.data
-        print(user)
-        print(psswd)
+        user = User.query.get(form.username.data).first()
+        if user and form.password.data == User.query.filter_by(username=form.username.data).first().password:
+            flash("sucess")
+        else:
+            flash("failed")
+
     return render_template("login.html", form=form)
 
 @app.route('/register',methods = ['GET','POST'])
 def register():
     form = RegisterForm()
     if request.method == 'POST':
-        user = form.username.data
-        print(user)
+        user = User(username = form.username.data,
+                    password = form.password.data,
+                    name = form.name.data,
+                    email = form.email.data ,
+                    phno = form.phone.data, 
+                    education_status = form.education.data ,
+                    field = form.field.data ,
+                    projects = form.project.data )
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
 
     return render_template("register.html",form=form)
