9404e66bf8bef01140187cbce57abb3eb0d31edd
Govind V R <govindvr02@gmail.com>
2022-01-28 23:09:56 +0530

    register form

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,5 @@
+from dataclasses import field
+from unicodedata import name
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
@@ -5,4 +7,11 @@ class RegisterForm(FlaskForm):
     username = StringField(label="Username")
     email = StringField(label = "Email")
     password = PasswordField(label="Password")
+    phone = StringField(label="Mobile Number")
+
+    name = StringField(label = "Name")
+    education = StringField(label = "Class")
+    field = StringField(label = "Area of Interest")
+    project = StringField(label = "Projects/Work")
+
     submit = SubmitField(label="Submit")
