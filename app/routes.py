d19df084cba2cda43f8ab1bce809136301cef07c
Govind V R <govindvr02@gmail.com>
2022-01-29 15:36:37 +0530

    templates updates

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from app.database import User
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
